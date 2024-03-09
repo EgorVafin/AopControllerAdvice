be870c09e66eb7531d3c462da72bae976ee711ba
Egor Vafin <post1978@gmail.com>
2024-03-09 13:44:33 +0300

    first commit

---
+++ src/main/java/com/example/aop_controlleradvice/AopControllerAdviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.aop_controlleradvice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopControllerAdviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AopControllerAdviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aop_controlleradvice/AopControllerAdviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aop_controlleradvice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopControllerAdviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
